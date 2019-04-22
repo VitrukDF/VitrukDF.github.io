94859121a28fd43b7a653f384262518fc2c42c56
VitrukDF <44525897+VitrukDF@users.noreply.github.com>
2019-04-22 22:22:53 +0300

    Add files via upload

---
+++ precache-manifest.1dccc94a6ce1c76be46a516a2c7e9ef2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f498b4c6b34df9e03be62e628407b467",
+    "url": "./static/media/tick.f498b4c6.svg"
+  },
+  {
+    "revision": "7fef4e83a2b8b261f849b0d0d30418b3",
+    "url": "./static/media/pluss.7fef4e83.svg"
+  },
+  {
+    "revision": "f2ca6631f80b85359bd6fe40e53a47b6",
+    "url": "./static/media/noughtMuted.f2ca6631.svg"
+  },
+  {
+    "revision": "fac8de417c8e58af85b6a6788cbcab59",
+    "url": "./static/media/nought.fac8de41.svg"
+  },
+  {
+    "revision": "57c94804c053ade7a5d934d86f21be70",
+    "url": "./static/media/cross.57c94804.svg"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "2b3dceb27840d43d6e1f",
+    "url": "./static/js/main.2b3dceb2.chunk.js"
+  },
+  {
+    "revision": "065ab0c36f4b05758964",
+    "url": "./static/js/2.065ab0c3.chunk.js"
+  },
+  {
+    "revision": "2b3dceb27840d43d6e1f",
+    "url": "./static/css/main.d06f9891.chunk.css"
+  },
+  {
+    "revision": "d1a88765d64ddfccd0d5b7ab011478eb",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c34fc054ee69bd7a1f53f34f721276c6.js"
+  "./precache-manifest.1dccc94a6ce1c76be46a516a2c7e9ef2.js"
 );
 
 workbox.clientsClaim();
